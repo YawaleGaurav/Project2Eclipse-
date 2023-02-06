602a6903f3eecc0804f3e62acbe5819c31244b79
Gaurav <gauravyawale28@gmail.com>
2023-02-06 18:16:49 +0530

    Demo1 file created and add value in HashSet

---
+++ CoreJava1/src/com/gaurav/programs/Demo1.java
@@ -0,0 +1,16 @@
+package com.gaurav.programs;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Set<Integer> set = new HashSet<Integer>();
+		set.add(20);
+		set.add(30);
+	}
+
+}
